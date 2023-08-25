ced5d47887d25ce00701f3907bc20ef02fec4997
DESKTOP-8DJJ3T5\user <lbsfe14@gmail.com>
2023-08-25 13:57:57 +0900

    HTML

---
+++ html-obentouya/html-obentouya/images/menu.php
@@ -0,0 +1,14 @@
+<header>
+        <div class="hero">
+            <img src="images/img08.png" alt="お店ロゴ" class="small-image">
+            <h1 class="logo">OH!弁当屋<span><br>幻のネット限定現地払い</span></h1>
+        </div>
+    <nav>
+        <ul>
+            <li><a href="#">お知らせ</a></li>
+            <li><a href="#">メニュー</a></li>
+            <li><a href="#">お支払い方法</a></li>
+            <li><a href="#" class="button">お問い合わせ</a></li>
+        </ul>
+    </nav>
+</header>
