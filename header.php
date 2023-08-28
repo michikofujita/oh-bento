7cbf3f82c80d2e2c97deaad4d46fc670e981fb87
DESKTOP-KB36TJC\liveb <lbsfe021@gmail.com>
2023-08-28 11:56:04 +0900

    藤田　ヘッダーのスマホメニュー部分　変更

--- header.php
+++ header.php
@@ -4,10 +4,13 @@
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>OH! 弁当屋</title>
     <link rel="stylesheet" href="reset.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="favicon" href="./favicon.ico">
-    <link rel="stylesheet" href="css/style-tb.css" media="screen and (max-width: 1024px)">
-    <link rel="stylesheet" href="css/style-sh.css" media="screen and (max-width: 599px)">
+
+    <!-- <link rel="stylesheet" href="css/news-style.css"> -->
+    <link rel="stylesheet" href="css/base-style.css">
+
+    <!-- <link rel="favicon" href="./favicon.ico">
+    <link rel="stylesheet" href="css/style-tb.css" media="screen and (min-width: 600px)">
+    <link rel="stylesheet" href="css/style-pc.css" media="screen and (min-width: 1000px)"> -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=M+PLUS+1p">   
 </head>
 
