1f913b8ed0bd37107f4c92722fb7fd0322dc64ec
LENOVO034\user <y.ninos2015@gmail.com>
2023-08-31 15:41:50 +0900

    修正・追加しました。平木

--- contact.php
+++ contact.php
@@ -1,95 +1,101 @@
-<!-- <?php require 'header.php'; ?>
-<?php require 'menu.php'; ?> -->
+<?php require 'header.php'; ?>
+<?php require 'menu.php'; ?>
+
+<head>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>OH! 弁当屋</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/base-style.css">
+    <link rel="stylesheet" href="css/contact-style.css">
+    <link rel="stylesheet" href="css/news-style.css">
+   
+
+    <link rel="favicon" href="./favicon.ico">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=M+PLUS+1p">   
+</head>
+
 
 <style>
-.contact {
-    width: 960px;
-    margin: 0 auto;
-    padding: 60px 0;
-}
-
-.contact-ttl {
-    font-size: 35px;
-    font-weight: bold;
-    margin-bottom: 40px;
-    text-align: center;
-    color: #85edb4;
-}
-
-.contact-table {
-    width: 100%;
-    margin-bottom: 20px;
-    border: 1px solid #ccc;
-}
-
-.contact-item,
-.contact-body {
-    padding: 20px;
-    border: 1px solid #ccc;
-}
-
-.contact-item {
-    text-align: left;
-    width: 30%;
-    background-color: #85edb4;
-}
-
-.contact-body {
-    text-align: left;
-    width: 70%;
-}
-
-.form-text {
-    width: 100%;
-    padding: 10px;
-    border-radius: 5px;
-    border: 1px solid #ccc;
-    appearance: none;
-    max-width: 400px;
-}
-
-.contact-sex + .contact-sex {
-    margin-left: 10px;
-}
-
-.contact-sex-txt {
-    display: inline-block;
-    margin-left: 5px;
-}
-
-.form-select {
-    width: 180px;
-    height: 40px;
-    padding: 5px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-}
-.form-textarea {
-    width: 100%;
-    padding: 10px;
-    height: 200px;
-    border-radius: 5px;
-    border: 1px solid #ccc;
-    appearance: none;
-    font-family: Arial, Helvetica, sans-serif;
-}
-
-.contact-submit {
-    width: 250px;
-    background-color: #85edb4;
-    color: #333333;
-    font-weight: bold;
-    display: block;
-    margin: 0 auto;
-    font-size: 16px;
-    padding: 15px;
-    border-radius: 100vh;
-    border: none;
-    appearance: none;
-    cursor: pointer;
-}
+/* .contact {
+            width: 960px;
+            margin: 0 auto;
+            padding: 60px 0;+
+        }
+        
+        .contact-ttl {
+            font-size: 35px;
+            font-weight: bold;
+            margin-bottom: 40px;
+            text-align: center;
+            color: #85edb4;
+        }
+        
+        .contact-table {
+            width: 100%;
+            margin-bottom: 20px;
+            border: 1px solid #ccc;
+        }
+        
+        .contact-item,
+        .contact-body {
+            padding: 20px;
+            border: 1px solid #ccc;
+        }
+        
+        .contact-item {
+            text-align: left;
+            width: 30%;
+            background-color: #85edb4;
+        }
+        
+        .contact-body {
+            width: 70%;
+        }
+        
+        .form-text {
+            width: 100%;
+            padding: 10px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+            appearance: none;
+            max-width: 400px;
+        }
+        
+            .form-select {
+            width: 180px;
+            height: 40px;
+            padding: 5px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+        
+        .form-textarea {
+            width: 100%;
+            padding: 10px;
+            height: 200px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+            appearance: none;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        
+        .contact-submit {
+            width: 250px;
+            background-color: #85edb4;
+            color: #333333;
+            font-weight: bold;
+            display: block;
+            margin: 0 auto;
+            font-size: 16px;
+            padding: 15px;
+            border-radius: 100vh;
+            border: none;
+            appearance: none;
+            cursor: pointer;
+        } */
 </style>
-<div class="contact">
+     <main id="contact">
+        <div class="contact">
             <h1 class="contact-ttl">Contact</h1>
             <form action="">
                 <table class="contact-table">
@@ -122,17 +128,17 @@
                             </label>
                          </td>
                     </tr>
-                    <tr>
-                        <th class="contact-item">お問い合わせ内容</th>
+                    <tr class="sample">
+                        <th class="contact-item ">お問い合わせ内容</th>
                         <td class="contact-body">
                             <textarea name="問い合わせ" class="form-textarea" placeholder="ご注文の変更につきましては、注文日時のご記入もあわせてお願いします。"></textarea>
                         </td>
                     </tr>
                 </table>
-
-                <input class="contact-submit" type="submit" value="送信" />
+                <input class="contact-submit" type="submit" value="送信" >
             </form>
         </div>
+    </main>
 
-        <?php require 'footer.php'; ?>
-
+<?php require 'address.php'; ?>
+<?php require 'footer.php'; ?>
