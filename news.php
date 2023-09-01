3c0dbca6a018315e778333bfa04fb74b9cbfdc51
DESKTOP-8DJJ3T5\user <lbsfe14@gmail.com>
2023-09-01 15:16:36 +0900

    手直し

--- news.php
+++ news.php
@@ -2,35 +2,44 @@
 <?php require 'menu.php'; ?>
 
 <main id="news">
+
   <div class="container">
-    <div class="news-box">
-      <h2>最新のお知らせ</h2>
-      <div class="news-item">
-        <p>2023年08月24日</p>
-        <h3>お弁当注文</h3>
-        <p></p>
-        <p>本日よりお弁当の注文受付可能になりました。</p>
-      </div>
-      <div class="news-item">
-        <p>2023年08月23日</p>
-        <h3>サイト開設のお知らせ</h3>
-        <p></p>
-        <p>本日よりOH！弁当屋のサイトをOPENしました。</p>
-      </div>
+    <div class="newsnews">
+      <h2 class="title">最新のお知らせ</h2>
+      <article class="newslist">
+        <p class="newsLost_date">2023年08月31日</p>
+        <p class="newsLost_tag">キャンペーン</p>
+        <p class="newsLost_title">初回限定50%OFF!!秋の特別キャンペーン。
+      </article>
+      <article class="newslist">
+        <p class="newsLost_date">2023年08月24日</p>
+        <p class="newsLost_tag">注文受付</p>
+        <p class="newsLost_title">本日よりお弁当の注文受付可能になりました。
+      </article>
+      <article class="newslist">
+        <p class="newsLost_date">2023年08月23日</p>
+        <p class="newsLost_tag">サイト開設</p>
+        <p class="newsLost_title">本日よりOH!弁当屋のサイトをOPENしました。
+      </article>
     </div>
 
-    <div class="b-radius">
-      <div class="item">
-        <figure class="image">
-          <img src="./images/news.phone.svg" alt="phone" />
-        </figure>
-        <p class="title">お電話でのお問い合わせ<br />0120-○○○○-○○○○</p>
-      </div>
-      <div class="item">
-        <figure class="image">
-          <img src="./images/news.e-mail.svg" alt="e-mail" />
-        </figure>
-        <p class="title">インターネットでの<br />お問い合わせ</p>
+    <div class="inner">
+      <div class="subfooter_inner">
+        <div class="subfooter_block">
+          <div class="subfooter_icon">
+            <img src="./images/news.phone.svg" alt="phone">
+          </div>
+              <p class="subfooter_title">お電話でのお問い合わせ</p>
+              <p class="subfooter_number">0120-○○○○-○○○○</p>
+        </div>
+
+        <div class="subfooter_block">
+          <div class="subfooter_icon">
+            <img src="./images/news.e-mail.svg" alt="e-mail">
+          </div>
+          <p class="subfooter_inquiry">インターネットでの
+            <br />お問い合わせ</p>
+        </div>
       </div>
     </div>
 
