307c71af915f72aaaa68fdc386d27e4959434a05
LENOVO034\user <y.ninos2015@gmail.com>
2023-09-07 13:24:26 +0900

    追加しました。

---
+++ confirm.php
@@ -0,0 +1,12 @@
+<?php require 'header.php'; ?>
+<?php require 'menu.php'; ?>
+
+   <!-- 商品 -->
+   <h3>お支払い方法</h3>
+<p class = "tyuui"><img src="images\image.png" alt=""width="350" height="150"></p>
+<p>お問合せ頂きありがとうございます。</p>
+<p>確認いたしますのでしばらくお待ちください。</p>
+
+   <!-- 商品 -->
+<?php require 'address.php'; ?>
+<?php require 'footer.php'; ?>
